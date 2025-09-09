1a2f02b85c0083f8708cc30503a08e3d7691f036
rupak choppala <rupakchoppala@gmail.com>
2025-09-09 17:37:32 +0530

    chnaging the paths of styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "/home/akshay/Documents/Product-Management-Application/client/src/styles/Global.css"
+import "./styles/Global.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
